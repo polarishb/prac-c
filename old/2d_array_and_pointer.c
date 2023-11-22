029f1cd7758bfe6352b5971e05ccc91e50c5d050
polarishb <polaris.hb03@gmail.com>
2023-11-23 01:00:17 +0900

    test

--- old/2d_array_and_pointer.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int arr[2][3] = { {1,2,3}, {4,5,6}};
-
-    printf("%p %p %p\n", arr, arr[0], &arr[0]);
-
-    return 0;
-}
--- old/my_print_functions.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void print_hello();
-void print_hi();
-void print_str(char* str);
